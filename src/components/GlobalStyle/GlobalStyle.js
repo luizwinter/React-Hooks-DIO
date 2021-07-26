a885600e2f1f573894076fc3b61abe684036b040
Luizwinter <junior.winter@gmail.com>
2021-07-26 12:49:02 -0300

    Inicio do projeto

---
+++ src/components/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        color: #332c36;
+        padding: 0;
+        margin: 0;
+
+    }
+ 
+`;
+
+export default GlobalStyle;
---
+++ src/pages/App/App.js
@@ -0,0 +1,6 @@
+function App(){
+    return <h1>Hello DIO</h1>
+
+};
+
+export default App;
